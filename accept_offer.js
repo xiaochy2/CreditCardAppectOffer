0aeb9e808618dcf5c5d1f5fe8d72bd2bb2135541
Tim Yang <xiaochy2@uci.edu>
2019-12-14 20:36:37 -0800

    Write script

---
+++ accept_offer.js
@@ -0,0 +1,41 @@
+/**
+ * Amex accept offer script
+ */
+const worker = setInterval(function() { 
+  const offer = document.querySelector('[title="Add to Card"]')
+  if (offer) {
+    offer.click() 
+  } else {
+    clearInterval(worker);
+    console.log('Done')
+  }
+}, 5000);
+
+
+/**
+ * Chase accept offer script
+ */
+const worker = setInterval(function() { 
+  const offer = document.querySelector('.sixersoffers__cta')
+  if (offer && offer.innerText === "Add to card") {
+    offer.click() 
+    setTimeout(function(){document.querySelector('#flyoutClose').click()},2000)
+  } else {
+    clearInterval(worker);
+    console.log('Done')
+  }
+}, 5000);
+
+
+/**
+ * Boa accept offer script
+ */
+const worker = setInterval(function() { 
+  const offer = document.querySelector('.add-deal')
+  if (offer) {
+    offer.click() 
+  } else {
+    clearInterval(worker);
+    console.log('Done')
+  }
+}, 400);
